cba6ec76b68733144711cc5f77cf8553c60b1b08
Jazzy Guo <guojazzy@gmail.com>
2023-05-01 19:12:18 -0400

    feat: cleaning up files

--- athena.py
+++ athena.py
@@ -1,19 +1,10 @@
 import subprocess
 import tempfile
 import ffmpeg
-from scipy.io import wavfile
-import math
 from fractions import Fraction
-import numpy as np
-import librosa
 import os
 from moviepy.video.io.VideoFileClip import VideoFileClip
-
-# number of s before and after
-seconds_to_capture = 30
-minimum_clips = 6
-maximum_clips = 12
-
+from audio_analyzer import get_loud_frames
 
 def extract_audio(input_file, output_file):
     command = f"ffmpeg -i '{input_file}' -ab 160k -ac 2 -ar 44100 -vn {output_file}"
@@ -27,132 +18,6 @@ def get_frame_rate(input_file):
     frame_rate = video_stream['avg_frame_rate']
     total_frames = int(probe["streams"][0]["nb_frames"])
     return float(Fraction(frame_rate)), total_frames
-
-
-def get_loud_frames(audio_file, frame_rate):
-    # the goal is to measure intervals of above avg decibel levels
-    # once we identify all intervals equal or above the threshold,
-    # we spread out the interval to match seconds_to_capture
-
-    # this states we are discovering db levels on this many second intervals
-    seconds_to_discover = 1
-
-    sample_rate, audio_data = wavfile.read(audio_file)
-    audio_sample_count = audio_data.shape[0]
-    samples_per_frame = sample_rate/frame_rate
-    audio_frame_count = int(math.ceil(audio_sample_count/samples_per_frame))
-
-    print('audio frame count', audio_frame_count)
-    print('samples per frame', samples_per_frame)
-
-    frame_audio_db_levels = []  # this stores frame by frame db levels
-    highest_db_level = -100
-
-    # calc highest db level and get db levels for each audio chunk on a frame by frame basis
-    for i in range(audio_frame_count):
-        start = int(i*samples_per_frame)
-        end = min(int((i+1)*samples_per_frame), audio_sample_count)
-        audio_chunk = audio_data[start:end]
-        rms = np.sqrt(np.mean(np.square(audio_chunk)))
-        db_level = librosa.power_to_db(rms, ref=1.0)
-        if math.isnan(db_level):
-            db_level = 0
-        frame_audio_db_levels.append(db_level)
-        highest_db_level = max(highest_db_level, db_level)
-
-    # we measure db levels on frame_intervals number of frames
-    results = []
-    current_frame = 0
-    highest_interval_db_avg = -100
-    total_db_intervals = 0
-    total_intervals = 0
-
-    frame_intervals = int(math.ceil(seconds_to_discover * frame_rate))
-
-    # we need enough frames to fill out a total of seconds_to_capture
-    frames_to_capture = int(math.ceil(
-        (seconds_to_capture * frame_rate) - frame_intervals
-    ) / 2)
-
-    # get highest average amongst all intervals
-    for i in range(0, audio_frame_count, frame_intervals):
-        interval_end_frame = min(audio_frame_count, i + frame_intervals)
-
-        # average of the current frame interval
-        interval_db_avg = sum(
-            frame_audio_db_levels[i:interval_end_frame]
-        ) / frame_intervals
-
-        highest_interval_db_avg = max(interval_db_avg, highest_interval_db_avg)
-
-        total_db_intervals += interval_db_avg
-        total_intervals += 1
-
-    total_avg_intervals = (total_db_intervals / total_intervals)
-    threshold = ((highest_interval_db_avg - total_avg_intervals) / 2) + \
-        (highest_db_level - highest_interval_db_avg)
-
-    print('The highest average db for an interval is', highest_interval_db_avg)
-    print('The highest db for a single frame is', highest_db_level)
-    print('Total average of all frame intervals', total_avg_intervals)
-    print(
-        f"Identifying frame intervals of {frame_intervals} to see which is the loudest"
-    )
-
-    while (not results or len(results) < minimum_clips):
-        print('threshold is currently', threshold)
-
-        for i in range(0, audio_frame_count, frame_intervals):
-            if (i <= current_frame):
-                continue
-
-            exists_in_results = False
-
-            for sublist in results:
-                if sublist['interval'][0] == i:
-                    exists_in_results = True
-
-            if exists_in_results:
-                continue
-
-            interval_end_frame = min(audio_frame_count, i + frame_intervals)
-
-            # average of the current frame interval
-            db_level = sum(
-                frame_audio_db_levels[i:interval_end_frame]
-            ) / frame_intervals
-
-            diff = db_level - total_avg_intervals
-
-            # print(
-            #     f"Frames {i} - {interval_end_frame} have a db level of {db_level}"
-            # )
-
-            if diff > threshold:
-                print(
-                    f"Frames {i} - {interval_end_frame} added as loud with db level of {db_level}"
-                )
-                # capture this amount of frames before and after current frame based on how many seconds
-                capture_end = min(audio_frame_count, i + frames_to_capture)
-
-                results.append({
-                    'interval': [i, interval_end_frame],
-                    'db_level': db_level
-                })
-
-                # update the loop starting index
-                current_frame = capture_end
-
-        threshold -= .1
-
-    if (len(results) > maximum_clips):
-        print(
-            f"Too many results - retrieving the {maximum_clips} loudest frames")
-        sorted_results = sorted(
-            results, key=lambda obj: obj['db_level'], reverse=True)
-        results = sorted_results[:maximum_clips]
-
-    return results, frames_to_capture
 
 
 def get_clip_frames(frames, total_frames, frames_to_capture):
---
+++ settings.py
@@ -0,0 +1,4 @@
+# number of s before and after
+seconds_to_capture = 30
+minimum_clips = 6
+maximum_clips = 12
